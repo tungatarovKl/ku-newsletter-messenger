fce36e5c3cba72b2d88e2b8cdf4f401c3444303f
DKBdg <strong.every@list.ru>
2022-11-12 23:02:13 +0600

    modified

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,18 +6,22 @@ import (
 	"net/http"
 
 	"github.com/BurntSushi/toml"
-	"gorm.io/driver/sqlite"
+	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
 
 type Config struct {
-	BotToken string
-	Dsn      string
+	BotToken   string
+	DbAddress  string `toml:"dbAddress"`
+	DbName     string `toml:"dbName"`
+	DbUsername string `toml:"dbUsername"`
+	DbPassword string `toml:"dbPassword"`
 }
 
 func handleNewsLetter(rw http.ResponseWriter, r *http.Request) {
 	rw.WriteHeader(http.StatusOK)
 }
+
 func main() {
 
 	configPath := flag.String("config", "", "Path to config file")
@@ -30,7 +34,8 @@ func main() {
 		log.Fatalf("Ошибка декодирования файла конфигов %v", err)
 	}
 
-	db, err := gorm.Open(sqlite.Open("../../"+cfg.Dsn), &gorm.Config{})
+	dsn := cfg.DbUsername + ":" + cfg.DbPassword + "@tcp(" + cfg.DbAddress + ")/" + cfg.DbName + "?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Fatalf("Ошибка подключения к БД %v", err)
