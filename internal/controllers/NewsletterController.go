a84a1219fde8588951675832eac30e506b3d30c8
Vladimir Kornilov <kornilov@kolesa.kz>
2022-11-15 02:03:42 +0600

    Изменил парсинг API- request с Form на Json Изменил настройки докера: Переименовал сервисы и контейнеры для более полного описания Добавил параметры сети для связи с фронтендом Убрал проброс порта 8080 на хост, т.к. запросы идут только из внутренней докер- сети

--- internal/controllers/NewsletterController.go
+++ internal/controllers/NewsletterController.go
@@ -1,23 +1,37 @@
 package controllers
 
 import (
+	"encoding/json"
+	"io"
 	"log"
 	"net/http"
 	"upgrade/internal/bot"
 )
 
+type ApiRequest struct {
+	Message string `json:"message"`
+	Token   string `json:"token"`
+}
+
 func NewsLetterPost(bot bot.Bot) http.HandlerFunc {
 	return func(rw http.ResponseWriter, r *http.Request) {
 
-		err := r.ParseForm()
-		if err != nil {
+		var apiRequest ApiRequest
+
+		requestBody, readErr := io.ReadAll(r.Body)
+		if readErr != nil {
 			http.Error(rw, "Request parsing error", http.StatusBadRequest)
 			return
 		}
 
-		message := r.Form.Get("message")
-		if message == "" {
-			http.Error(rw, "'message' key required'", http.StatusBadRequest)
+		parseErr := json.Unmarshal(requestBody, &apiRequest)
+		if parseErr != nil {
+			http.Error(rw, "Request parsing error", http.StatusBadRequest)
+			return
+		}
+
+		if apiRequest.Message == "" || apiRequest.Token == "" {
+			http.Error(rw, "Some required fields are absent", http.StatusBadRequest)
 			return
 		}
 
@@ -30,7 +44,7 @@ func NewsLetterPost(bot bot.Bot) http.HandlerFunc {
 
 		//Send message for all registered users
 		for _, user := range users {
-			bot.SendMessage(user.TelegramId, message)
+			bot.SendMessage(user.TelegramId, apiRequest.Message)
 		}
 
 		rw.WriteHeader(http.StatusOK)
